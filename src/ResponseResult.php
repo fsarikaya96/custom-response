1b9b489c894c2e9ce4f78df066cce58d96b32d59
fsarikaya96 <fsarikaya96@hotmail.com>
2023-01-25 22:49:52 +0300

    refactor : response codes and response result updated

--- src/ResponseResult.php
+++ src/ResponseResult.php
@@ -4,12 +4,12 @@ namespace Justfeel\Response;
 
 class ResponseResult
 {
-    public static function generate(bool $success = true, $message = null, int $errorcode = 200) : object
+    public static function generate(bool $success = true, $message = null, int $statusCode = 200) : object
     {
         if ($success) {
-            return response()->json(['success' => $success, 'message' => $message], $errorcode);
+            return response()->json(['success' => $success, 'message' => $message], $statusCode);
         } else {
-            return response()->json(['success' => $success, 'errorMessage' => $message, 'errorCode' => $errorcode],$errorcode);
+            return response()->json(['success' => $success, 'errors' => $message, 'errorCode' => $statusCode],$statusCode);
         }
     }
 }
